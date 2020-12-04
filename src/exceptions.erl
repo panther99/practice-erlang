ac9b772b7225ed8c6b597028ab48b5c302c376b1
panther99 <nikola.stojakovic@hotmail.com>
2020-12-05 00:04:54 +0100

    Add module for practicing exceptions

---
+++ src/exceptions.erl
@@ -0,0 +1,64 @@
+-module(exceptions).
+
+-export([
+    throws/1,
+    errors/1,
+    exits/1,
+    black_knight/1,
+    talk/0,
+    sword/1,
+    whoa/0
+]).
+
+throws(F) ->
+    try F() of
+        _ -> ok
+    catch
+        Throw -> {throw, caught, Throw}
+    end.
+
+errors(F) ->
+    try F() of
+        _ -> ok
+    catch
+        error:Error -> {error, caught, Error}
+    end.
+
+exits(F) ->
+    try F() of
+        _ -> ok
+    catch
+        exit:Exit -> {exit, caught, Exit}
+    end.
+
+sword(1) -> throw(slice);
+sword(2) -> erlang:error(cut_arm);
+sword(3) -> exit(cut_leg);
+sword(4) -> throw(punch);
+sword(5) -> exit(cross_bridge).
+
+black_knight(Attack) when is_function(Attack, 0) ->
+    try Attack() of
+        _ -> "None shall pass."
+    catch
+        throw:slice -> "It is but a scratch.";
+        error:cut_arm -> "I've had worse.";
+        exit:cut_leg -> "Come on you pansy!";
+        _:_ -> "Just a flesh wound."
+    end.
+
+talk() -> "blah blah".
+
+whoa() ->
+    try
+        talk(),
+        _Knight = "None shall pass!",
+        _Doubles = [N * 2 || N <- lists:seq(1, 100)],
+        exit(bye),
+        throw(up),
+        _WillReturnThis = tequilla
+    of
+        tequilla -> "hey this worked!"
+    catch
+        Exception:Reason -> { caught, Exception, Reason }
+    end.
