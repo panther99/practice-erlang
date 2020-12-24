5fca1a54aa34aa51b2ab1321c7c1c7fa631d77b4
panther99 <nikola.stojakovic@hotmail.com>
2020-12-24 22:40:43 +0100

    Add an example for error propagation in a chain of linked processes

--- src/concurrency/link.erl
+++ src/concurrency/link.erl
@@ -3,7 +3,8 @@
 
 -export([
    spawn_parent_proc/0,
-   spawn_parent_proc_atomically/0
+   spawn_parent_proc_atomically/0,
+   chain/1
 ]).
 
 % A process which will wait 5 seconds before dying.
@@ -24,3 +25,20 @@ spawn_parent_proc() ->
 spawn_parent_proc_atomically() ->
     Pid = spawn_link(fun proc/0),
     link(Pid).
+
+% After process dies, process which are linked with it will receive
+% it's "death signal" which will be propagated throughout the chain
+
+chain(0) ->
+    receive
+        _ -> ok
+    after 2000 ->
+        exit("chain dies here")
+    end;
+
+chain(N) ->
+    Pid = spawn(fun () -> chain(N - 1) end),
+    link(Pid),
+    receive
+        _ -> ok
+    end.
